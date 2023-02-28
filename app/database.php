68747173775786ca7044002404f9699e19961c71
Travisrulez <mazaevroma@gmail.com>
2023-03-01 00:07:49 +0300

    update db

--- app/database.php
+++ app/database.php
@@ -1,6 +1,6 @@
 <?php
 mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
-$link = mysqli_connect('localhost', 'root', 'root', 'diplom');
+$link = mysqli_connect('std-mysql', 'std_939_diplom', 'tkfynjdj1', 'std_939_diplom');
 
 if (mysqli_connect_errno()) {
     echo 'Ошибка подключения к базе данных ('.mysqli_connect_errno().')'.mysqli_connect_error();
